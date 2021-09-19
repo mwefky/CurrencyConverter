177b7ae7d611c2369dd1ca9343c1239d7211f7f8
wefky <minawefky3@gmail.com>
2021-09-19 12:00:54 +0200

    unit tests added

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -6,28 +6,41 @@
 //
 
 import XCTest
+import RxSwift
 @testable import CurrencyConverter
 
 class CurrencyConverterTests: XCTestCase {
 
+    var mockNetwork: ApiClient!
+    let disposeBag = DisposeBag()
+    var currencyViewModel = CurrencyConversionViewModel()
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockNetwork = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testValidateNetWorkCallsForVenue() {
+        
+        let promise = expectation(description: "Status code: 200")
+        ApiClient.convertCurrency(amount: 20, fromCur: "USD", toCur: "EUR")
+            .subscribe(onNext: { _ in
+                promise.fulfill()
+            }, onError: { (error) in
+                XCTFail("Error: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
+        wait(for: [promise], timeout: 5)
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
